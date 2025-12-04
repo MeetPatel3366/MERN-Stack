1babedcdad2a765579338421c8c48592cbb45674
Meet Patel <meet3366patel@gmail.com>
2025-12-04 15:05:17 +0530

    js : basics

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+console.log("Hello World")
+let p=document.getElementById("demo2")
+function myFunction()
+{
+    p.style.backgroundColor="red";
+}
+
+// single line comment 
+/* multi 
+line 
+comment*/
