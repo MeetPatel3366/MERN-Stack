255ff1950b22be1ca2996119ac603caf5065e2c7
Meet Patel <meet3366patel@gmail.com>
2025-12-06 14:30:02 +0530

    task: react-redux and redux-toolkit

---
+++ react/third/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Couter from './Couter'
+
+const App = () => {
+  return (
+    <div>
+      <Couter/>
+    </div>
+  )
+}
+
+export default App
---
+++ react/third/src/Couter.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {useDispatch, useSelector} from "react-redux";
+import { increment,decrement, incrementByAmount } from './Redux/slices/counterSlice';
+
+const Couter = () => {
+    const count=useSelector((state)=>state.counter.value)
+    const dispatch=useDispatch()
+  return (
+    <div>
+      <button onClick={()=>dispatch(increment())}>Increment</button>
+      <span>{count}</span>
+      <button onClick={()=>dispatch(decrement())}>Decrement</button>
+      <button onClick={()=>dispatch(incrementByAmount(5))}>Increment By Five</button>
+    </div>
+  )
+}
+
+export default Couter
