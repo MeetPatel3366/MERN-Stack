65c5cdb174838fae2b4c96aab1a7eef89eaf69ee
Meet Patel <meet3366patel@gmail.com>
2025-12-06 16:59:54 +0530

    nodejs: import - export modules

---
+++ nodejs/index.js
@@ -0,0 +1,13 @@
+//npm init
+//npm init -y
+
+//-------------------
+
+// const { myObj } = require("./modules/firstModule");
+// console.log(myObj);
+
+
+//--------------
+
+import { myObj } from "./modules/firstModule";
+console.log(myObj);
---
+++ nodejs/modules/firstModule.js
@@ -0,0 +1,11 @@
+// exports.myObj={
+//     name:"Demo_1",
+//     dev:true
+// }
+
+// console.log(myObj);
+
+export const myObj={
+    name:"demo2",
+    age:20
+}
